2bedf49c8b9fc33d7c5bc7df16373db238251fb2
Nite <nitearie@gmail.com>
2019-12-10 10:36:38 +0700

    Updates

---
+++ precache-manifest.715186be16e56b47db78a080c3f3117b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abaeb2cedfcb6fea8ae6904c428341a",
+    "url": "/cat-fact/index.html"
+  },
+  {
+    "revision": "89a5f474cff96843a88f",
+    "url": "/cat-fact/static/css/main.2b8860c0.chunk.css"
+  },
+  {
+    "revision": "801b7a09f26958839292",
+    "url": "/cat-fact/static/js/2.3b75785a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cat-fact/static/js/2.3b75785a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89a5f474cff96843a88f",
+    "url": "/cat-fact/static/js/main.9ebac9ad.chunk.js"
+  },
+  {
+    "revision": "4cb761853cdfa66243e0",
+    "url": "/cat-fact/static/js/runtime-main.5e32c1e6.js"
+  }
+]);
